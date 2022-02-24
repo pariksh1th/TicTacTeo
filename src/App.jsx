10f650c5c97534b4be0aab169d9bdf901fe9a2ef
Parikshith Palegar <parikshith@Parikshiths-MacBook-Air.local>
2022-02-24 20:42:56 +0530

    added history

--- src/App.jsx
+++ src/App.jsx
@@ -4,35 +4,40 @@ import Board from './Board';
 import calculateWinner from './winner';
 
 function App() {
-  const [board, setBoard] = useState(Array(9).fill(null));
-  const [isNext, setIsNext] = useState(false);
-
-  const winner = calculateWinner(board);
+  const [history, setHistory] = useState([
+    { board: Array(9).fill(null), isNext: true },
+  ]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const current = history[currentMove];
+  const winner = calculateWinner(current.board);
   console.log(winner);
+  console.log(current);
 
   const massage = winner
     ? `Winner is ${winner} `
-    : `Next palyer is ${isNext ? 'X' : 'O'}`;
+    : `Next palyer is ${current.isNext ? 'X' : 'O'}`;
 
   const handleClick = position => {
-    if (board[position]) return;
-    setBoard(perv => {
-      return perv.map((Square, pos) => {
+    if (current.board[position] || winner) return;
+    setHistory(perv => {
+      const last = perv[perv.length - 1];
+      const newBoard = last.board.map((Square, pos) => {
         if (position === pos) {
-          return isNext ? 'X' : 'O';
+          return last.isNext ? 'X' : 'O';
         }
         return Square;
       });
+      return perv.concat({ board: newBoard, isNext: !last.isNext });
     });
 
-    setIsNext(perv => !perv);
+    setCurrentMove(perv => perv + 1);
   };
 
   return (
     <div className="app">
       <h1>TIC TAC TOE</h1>
       <h2>{massage}</h2>
-      <Board board={board} handleClick={handleClick} />
+      <Board board={current.board} handleClick={handleClick} />
     </div>
   );
 }
