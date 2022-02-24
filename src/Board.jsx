9e4d86cac19a673a4465ea9f86a166a8d702ccc5
Parikshith Palegar <parikshith@Parikshiths-MacBook-Air.local>
2022-02-24 18:25:48 +0530

    add basic styles and setup

---
+++ src/Board.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import Square from './Square'
+
+function Board() {
+
+
+    const [board, setBoard] = useState(Array(9).fill(null))
+
+    return (
+        <div className='board'>
+            <div className='board-row'>
+                <Square value={board[0]} />
+                <Square value={board[1]} />
+                <Square value={board[2]} />
+            </div>
+            <div className='board-row'>
+                <Square value={board[3]} />
+                <Square value={board[4]} />
+                <Square value={board[5]} />
+
+            </div>
+            <div className='board-row'>
+                <Square value={board[6]} />
+                <Square value={board[7]} />
+                <Square value={board[8]} />
+
+            </div>
+        </div>
+    )
+}
+
+export default Board
---
+++ src/Square.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Square(props) {
+    return (
+        <button className='square'>{props.value}</button>
+    )
+}
+
+export default Square
