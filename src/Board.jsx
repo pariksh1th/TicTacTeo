417529389919151396f8a4ba77405d049e8b232b
Parikshith Palegar <parikshith@Parikshiths-MacBook-Air.local>
2022-02-24 18:58:33 +0530

    added board state

--- src/Board.jsx
+++ src/Board.jsx
@@ -5,24 +5,43 @@ function Board() {
 
 
     const [board, setBoard] = useState(Array(9).fill(null))
+    const [isNext, setIsNext] = useState(false)
 
+    const handleClick = position => {
+
+        if (board[position]) return
+        setBoard((perv) => {
+            return perv.map((Square, pos) => {
+                if (position === pos) {
+                    return isNext ? "X" : "O"
+                }
+                return Square
+            })
+        })
+
+        setIsNext(perv => !perv)
+    }
+
+    const renderSquare = pos => {
+        return <Square value={board[pos]} clicks={handleClick} position={pos} />
+    }
     return (
         <div className='board'>
             <div className='board-row'>
-                <Square value={board[0]} />
-                <Square value={board[1]} />
-                <Square value={board[2]} />
+                {renderSquare(0)}
+                {renderSquare(1)}
+                {renderSquare(2)}
             </div>
             <div className='board-row'>
-                <Square value={board[3]} />
-                <Square value={board[4]} />
-                <Square value={board[5]} />
+                {renderSquare(3)}
+                {renderSquare(4)}
+                {renderSquare(5)}
 
             </div>
             <div className='board-row'>
-                <Square value={board[6]} />
-                <Square value={board[7]} />
-                <Square value={board[8]} />
+                {renderSquare(6)}
+                {renderSquare(7)}
+                {renderSquare(8)}
 
             </div>
         </div>
--- src/Square.jsx
+++ src/Square.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Square(props) {
     return (
-        <button className='square'>{props.value}</button>
+        <button onClick={() => {
+            props.clicks(props.position)
+        }} className='square'>{props.value}</button>
     )
 }
 
